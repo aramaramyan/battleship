2363f2b1a5f4fc27c91916e850f25e176669db56
aramaramyan <aramaramyan1997@mail.ru>
2022-04-28 17:39:19 +0400

    feat: create board

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+
+const board = Array.from(Array(100).keys());
+const grouping = board.reduce((acc, item, index) => {
+  const currentIndex = index % 10;
+  (acc[currentIndex] = acc[currentIndex] || []).push(item);
+  return acc;
+}, []);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {grouping.map(row => <div className="row">
+        {row.map(square => <div className="square"></div>)}
+      </div>)}
+    </>
   );
 }
 
